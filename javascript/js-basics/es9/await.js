23c64c7d4320449f9d4126be0606d8ccafb9be0b
Cosmin C <concosminx@users.noreply.github.com>
2021-12-27 14:35:31 +0200

    es9 added + promises + async await

---
+++ javascript/js-basics/es9/await.js
@@ -0,0 +1,61 @@
+// async await transformation
+fetch('https://swapi.co/api/starships/9/')
+  .then(response => response.json())
+  .then(console.log)
+
+async function fetchStarship() {
+  const response = await fetch('https://swapi.co/api/starships/9/')
+  const data = await response.json();
+  console.log(data);
+}
+
+// #2) ADVANCED: Update the function below from the video to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+//example
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+      const response = await fetch(url);
+      return response.json();
+  }));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+
+// try catch block
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholdeTYPO.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  try {
+    const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+        const response = await fetch(url);
+        return response.json();
+    }));
+    console.log('users', users);
+    console.log('posta', posts);
+    console.log('albums', albums);
+  } catch (err) {
+    console.log('ooooooops', err);
+  }
+}
