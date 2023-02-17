b59a57c250929728943187862a1eb8b9e7d1c050
Cosmin C <concosminx@users.noreply.github.com>
2023-02-17 08:09:46 +0200

    Added images

--- node/express-project/controllers/messages.controller.js
+++ node/express-project/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-  res.send('<ul><li>Hello my friend!</li></ul>');
+  // res.send('<ul><li>Hello my friend!</li></ul>');
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'ski.png'));
 }
 
 function postMessage(req, res) {
--- node/express-project/index.js
+++ node/express-project/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const messageController = require('./controllers/messages.controller');
-const friendsController = require('./controllers/friends.controller');
+const path = require('path');
+
+const { friendsRouter } = require('./routes/friends.router');
+const { messageRouter } = require('./routes/messages.router');
 
 const app = express();
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -10,28 +13,14 @@ app.use((req, res, next) => {
   next();
   //actions go here
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta} ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-// app.get('/', (req,res) => {
-//   //res.send('Hello world!');
-//   res.send({
-//     id: 2, 
-//     name: 'Isaac Newton'
-//   });
-// });
-
-app.get('/friends', friendsController.getFriends);
-
-app.get('/friends/:friendId', friendsController.getFriend);
-
-app.post('/friends', friendsController.postFriend);
-
-app.get('/messages', messageController.getMessages);
-
-app.post('/messages', messageController.postMessage);
+app.use('/friends', friendsRouter); 
+app.use('/messages', messageRouter); 
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
